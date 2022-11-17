4d76f766848c1c848c613414eb6bcbb063f2e7d5
meganemolero <megane.romary@yahoo.fr>
2022-11-17 19:32:36 +0100

    modification de la balise banner en div ==> visuel récupéré !

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,12 +4,12 @@ import BannerPhoto from '../../datas/Images/ImgBannerHome.png';
 <div>Coucou</div>
 export default function Banner() {
     return(
-        <Banner className={BannerCSS.banner}>
+        <div className={BannerCSS.banner}>
             <div className={BannerCSS.bannerContent}>
                 <img className={BannerCSS.img} src={ BannerPhoto} alt='Falaise en bord de mer'/>
                 <h1 className={BannerCSS.title}>Chez vous, partout et ailleurs</h1>
             </div>
-        </Banner>
+        </div>
     )
 }
 
