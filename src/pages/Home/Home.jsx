8c6d6b02deb7906fcb2a65657c476a434d992b37
meganemolero <megane.romary@yahoo.fr>
2022-12-05 14:39:30 +0100

    modifications

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import Banner from '../../components/Banner/Banner'
 import Card  from '../../components/Card/Card'
-import Lodgings from '../../datas/lodging.json'
+
 import './Home.css'
 
 import BannerHomePhoto from '../../datas/Images/ImgBannerHome.png'
@@ -13,19 +13,7 @@ export default function Home() {
             alt="Falaises en bord de mer"
             title={"Chez vous, partout et ailleurs"}
             />
-            <section className='homeCard'>
-                <div className='homeCardContainer'>
-                    {Lodgings.map((logements) => (
-                        <Card
-                            key={logements.id}   
-                            title={logements.title}
-                            Link={`/lodgings/`+ logements.id}
-                            cover={logements.cover}
-                        >   
-                        </Card>
-                    ))}
-                </div>
-            </section>    
+            <Card />
         </div>
     )
 }
