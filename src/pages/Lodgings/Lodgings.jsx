f9d69656ccdc35e00032a9052ac9baf78d416327
meganemolero <megane.romary@yahoo.fr>
2022-12-06 10:29:53 +0100

    work on page lodgings

--- src/pages/Lodgings/Lodgings.jsx
+++ src/pages/Lodgings/Lodgings.jsx
@@ -1,31 +1,47 @@
+/*Import du fichier CSS*/
 import './Lodgings.css'
+/*Import des composants nécessaires à la construction de la page*/
 import Slideshow from '../../components/Slideshow/Slideshow'
 import LodgingDetails from '../../components/LodgingDetails/LodgingDetails'
 import Collapse from '../../components/Collapse/Collapse'
-
+/*Import des Hooks nécessaires*/
 import { useParams } from "react-router-dom"
+import {useState} from "react"
+/*Import du fichier Json contenant les informations des logements*/
 import Logements from "../../datas/lodging.json"
 
 
-
+/*Fonction Lodgings*/
 export default function Lodgings() {
-    let { id }= useParams();
-    const LogId = ({id})
+    /*Récupération de l'id contenu dans l'URL*/
+    let params = useParams();
+    const [lodgings, setLodgings] = useState(Logements.find(e => {
+        return e.id === params.id
+    }))
+    
+    const images = [];
+    lodgings.images.forEach((image) => {
+        images.push(image);
+    });
+    const equipments = lodgings.equipments;
 
-    const oneLodging = Logements.find(e => e.id ===LogId.id)
-    const equipments = oneLodging.equipments;
+
     return (
         <div className="LodgingsContainer">
-            <Slideshow key={oneLodging.pictures} photos={oneLodging.pictures}/>
-            <LodgingDetails key={oneLodging.title} lodgingDetails={oneLodging}/>
+            <Slideshow 
+                key={lodgings.pictures} 
+                photos={lodgings.pictures}/>
+            <LodgingDetails 
+                key={lodgings.title} 
+                lodgingDetails={lodgings}/>
             <div className="LodgingsCollapse">
                 <Collapse 
-                    key={oneLodging.description}
+                    key={lodgings.description}
                     title="Description"
-                    content={oneLodging.description}
+                    content={lodgings.description}
                     />
                 <Collapse 
-                    key={oneLodging.equipments}
+                    key={lodgings.equipments}
                     title="Equipements"
                     content={equipments}    
                 />
