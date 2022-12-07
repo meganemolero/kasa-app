b9b0094484afe2143dd1109a72c6060b325bbe5f
meganemolero <megane.romary@yahoo.fr>
2022-12-07 14:53:36 +0100

    ajustements

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,22 +1,20 @@
-import './Card.css'
+
 
 export default function Card(props) {
     return (
-            <>
-                <figure 
-                    key={props.logements.id}
-                    onClick={() => window.location.href = `/lodgings/${props.logements.id}`}>
-                    <img 
-                        src={props.logements.cover} 
-                        alt={props.logements.title }
-                    />
-                    <figcaption>
-                        <h2>
-                            {props.logements.title}
-                        </h2>
-                    </figcaption>   
-                </figure>
-            </>
+            <figure 
+                key={props.logements.id}
+                onClick={() => window.location.href = `/lodgings/${props.logements.id}`}>
+                <img 
+                    src={props.logements.cover} 
+                    alt={props.logements.title }
+                />
+                <figcaption>
+                    <h2>
+                    {props.logements.title}
+                    </h2>
+                </figcaption>   
+            </figure>    
     )
 }
 
